80fda9cfc2f75d1dbd08801bc8166ed7f0171c15
ameth <amethyst.asuka@gcmodeller.org>
2016-04-27 19:53:06 +0800

    too slow.......

--- ImageCompare/Compares.vb
+++ ImageCompare/Compares.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.DataMining.Framework.DynamicProgramming
 Imports Microsoft.VisualBasic.Linq
 
 ''' <summary>
@@ -10,6 +11,13 @@ Public Module Compares
     Public Function Compares(seq1 As ImageSequence, seq2 As ImageSequence) As Rectangle()
         Dim MAT1 As Single()() = seq1.Rows.ToArray(Function(x) x.ToArray(Function(cl) cl.GetBrightness))
         Dim MAT2 As Single()() = seq2.Rows.ToArray(Function(x) x.ToArray(Function(cl) cl.GetBrightness))
+        Dim gsw As New GSW(Of Single())(MAT1, MAT2, AddressOf Similarity, Function(x) Chr(x.Average))
+
+    End Function
+
+    Private Function Similarity(a As Single(), b As Single()) As Integer
+        Dim gsw As New GSW(Of Single)(a, b, Function(x, y) 255 - (Math.Abs(x - y)), Function(x) Chr(x))
+        Return gsw.AlignmentScore
     End Function
 
     Public Function Compares(seq1 As Bitmap, seq2 As Bitmap) As Rectangle()
--- ImageCompare/Program.vb
+++ ImageCompare/Program.vb
@@ -1,7 +1,8 @@
-﻿Module Program
+﻿Imports System.Drawing
+
+Module Program
 
     Sub Main()
-
+        Dim rsult = Compares.Compares(Image.FromFile("F:\asuka\computer-vision\data\a.png"), Image.FromFile("F:\asuka\computer-vision\data\b.png"))
     End Sub
-
 End Module
