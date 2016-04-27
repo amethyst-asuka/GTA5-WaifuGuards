0a294d83098c939e6d2ce5fcc98df0ded978c15e
ameth <amethyst.asuka@gcmodeller.org>
2016-04-27 10:05:02 +0800

    init projects of image difference compares

---
+++ ImageCompare/Compares.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Linq
+
+''' <summary>
+''' 得到两个图片之后，进行动态规划比较差异
+''' 1. 首先进行逐行扫描，
+''' </summary>
+Public Module Compares
+
+    Public Function Compares(seq1 As ImageSequence, seq2 As ImageSequence) As Rectangle()
+        Dim MAT1 As Single()() = seq1.Rows.ToArray(Function(x) x.ToArray(Function(cl) cl.GetBrightness))
+        Dim MAT2 As Single()() = seq2.Rows.ToArray(Function(x) x.ToArray(Function(cl) cl.GetBrightness))
+    End Function
+
+    Public Function Compares(seq1 As Bitmap, seq2 As Bitmap) As Rectangle()
+        Return Compares(New ImageSequence(seq1), New ImageSequence(seq2))
+    End Function
+End Module
---
+++ ImageCompare/ImageSequence.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Serialization
+
+Public Class ImageSequence
+    Implements IEnumerable(Of Color)
+
+    ''' <summary>
+    ''' 这个图片对象在原始图片上面的位置
+    ''' </summary>
+    Public ReadOnly RECT As Rectangle
+    Public ReadOnly Property Raw As Color()
+    Public ReadOnly Property Rows As IEnumerable(Of Color())
+
+    Sub New(bitmap As hBitmap, rect As Rectangle)
+        Me.RECT = rect
+
+        Dim index As Integer = bitmap.GetIndex(rect.X, rect.Y)
+        Dim len As Integer = rect.ByteLength
+        Dim buf As Byte() = New Byte(len - 1) {}
+
+        Array.ConstrainedCopy(bitmap.Raw, index, buf, Scan0, buf.Length)
+        Raw = buf.Colors.ToArray
+        Rows = Raw.Split(rect.Width).ToArray
+    End Sub
+
+    Sub New(bitmap As hBitmap)
+        Call Me.New(bitmap, New Rectangle(New Point, bitmap.Size))
+    End Sub
+
+    Sub New(bitmap As Bitmap)
+        Call Me.New(hBitmap.FromBitmap(bitmap))
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return RECT.GetJson
+    End Function
+
+    Public Iterator Function GetEnumerator() As IEnumerator(Of Color) Implements IEnumerable(Of Color).GetEnumerator
+        For Each x As Color In Raw
+            Yield x
+        Next
+    End Function
+
+    Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Yield GetEnumerator()
+    End Function
+End Class
