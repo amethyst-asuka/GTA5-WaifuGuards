54f07d4408ea4cc8802856373391b5e6904e40a4
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-04-01 19:12:15 +0800

    add script code

---
+++ WaifuGuards/Waifu.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Windows.Forms
+Imports GTA
+Imports GTA.Math
+
+Public Class Waifus : Inherits Script
+
+    ReadOnly names$() = {
+        "nelliel",
+        "ram",
+        "rem",
+        "rmiku2014",
+        "rmiku2015",
+        "rmiku2016",
+        "tohka",
+        "tohru",
+        "yoshino",
+        "beatrice",
+        "kanna",
+        "kotori",
+        "megumin",
+        "megumin2"
+    }
+    ReadOnly rand As New Random
+    ReadOnly waifuGuards As New List(Of Ped)
+
+    Dim lastCheck As DateTime = Now
+
+    Shared ReadOnly twoSecond As New TimeSpan(0, 0, 2)
+
+    Private Sub spawnWaifu(name As String)
+        Dim waifu As Ped = World.CreatePed(New Model(name), Game.Player.Character.GetOffsetInWorldCoords(New Vector3(rand.Next(1, 5), rand.Next(1, 5), 0)))
+
+        waifu.Weapons.Give(Native.WeaponHash.StunGun, 9999, True, True)
+        waifu.RelationshipGroup = Game.Player.Character.RelationshipGroup
+        waifu.MaxHealth = 10000
+        waifu.Armor = 10000
+
+        waifuGuards.Add(waifu)
+    End Sub
+
+    Private Sub Waifus_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Y Then
+            Call spawnWaifu(names(rand.Next(0, names.Length)))
+        End If
+    End Sub
+
+    Private Sub Waifus_Tick(sender As Object, e As EventArgs) Handles Me.Tick
+        If (Now - lastCheck) >= twoSecond Then
+            For Each waifu In waifuGuards
+                If waifu.IsDead Then
+                    waifu.Delete()
+                Else
+                    If Game.Player.Character.IsRunning Then
+                        waifu.Task.RunTo(Game.Player.Character.Position, False)
+                    Else
+                        waifu.Task.GoTo(Game.Player.Character, New Vector3(rand.Next(1, 5), rand.Next(1, 5), 0))
+                    End If
+                End If
+            Next
+
+            lastCheck = Now
+        End If
+
+        For Each waifu In waifuGuards
+            If Not waifu.IsDead Then
+                waifu.Health = 100
+            End If
+        Next
+    End Sub
+End Class
