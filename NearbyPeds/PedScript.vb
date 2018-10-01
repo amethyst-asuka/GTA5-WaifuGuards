538ec7443840d5b086d3143e9f431e5a9305575d
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-10-02 05:21:59 +0800

    get all peds that nearby player

--- NearbyPeds/PedScript.vb
+++ NearbyPeds/PedScript.vb
@@ -1,3 +1,26 @@
-﻿Public Class PedScript
+﻿Public Class PedScript : Inherits Script
 
+    ReadOnly Skins$() = {
+        "tonacho",
+        "ByStaxx",
+        "Capitan_america",
+        "DeadPool",
+        "ElRubius",
+        "empollon",
+        "LuzuGames",
+        "perxittaa",
+        "Tigre"
+    }
+    ReadOnly rand As New Random
+    ReadOnly player As Integer = Game.Player.Character.RelationshipGroup
+
+    Dim nearbyPeds As List(Of Ped)
+    Dim distance As Double = 500
+
+    Private Sub PedScript_Tick(sender As Object, e As EventArgs) Handles Me.Tick
+        Dim nearby As Ped() = World _
+            .GetNearbyPeds(Game.Player.Character.Position, distance) _
+            .Where(Function(p) p.RelationshipGroup <> player) _
+            .ToArray
+    End Sub
 End Class
