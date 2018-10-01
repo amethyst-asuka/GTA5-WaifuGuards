517d2486bb729896160c6d4befd08892e09a5e54
关倩倩 <evia@lilithaf.me>
2018-10-01 17:40:39 +0800

    fix task bugs

--- WaifuGuards/Events/FollowPlayer.vb
+++ WaifuGuards/Events/FollowPlayer.vb
@@ -20,18 +20,20 @@ Public Class FollowPlayer : Inherits TickEvent
 
             ' If your waifu is too far away with player, then your waifus will running to you
             ' else walking
-            If waifu.DistanceToPlayer >= 10 Then
-                Call waifu.TakeAction(
-                    Sub(action As Tasks)
-                        Call action.ClearAllImmediately()
-                        Call action.RunTo(Game.Player.Character.Position, False)
-                    End Sub)
-            Else
-                Call waifu.TakeAction(
-                    Sub(action As Tasks)
-                        Call action.ClearAllImmediately()
-                        Call action.GoTo(Game.Player.Character, offset)
-                    End Sub)
+            If Not waifu.IsInCombat Then
+                If waifu.DistanceToPlayer >= 10 Then
+                    Call waifu.TakeAction(
+                        Sub(action As Tasks)
+                            Call action.ClearAllImmediately()
+                            Call action.RunTo(Game.Player.Character.Position, False)
+                        End Sub)
+                Else
+                    Call waifu.TakeAction(
+                        Sub(action As Tasks)
+                            Call action.ClearAllImmediately()
+                            Call action.GoTo(Game.Player.Character, offset)
+                        End Sub)
+                End If
             End If
         Next
     End Sub
