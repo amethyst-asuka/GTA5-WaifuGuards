ce1e0ec9215b4bc82066328ddc951c9b61b01a8d
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-10-02 05:30:35 +0800

    change model of the nearby ped

--- NearbyPeds/PedScript.vb
+++ NearbyPeds/PedScript.vb
@@ -1,4 +1,6 @@
-﻿Public Class PedScript : Inherits Script
+﻿Imports System.Runtime.CompilerServices
+
+Public Class PedScript : Inherits Script
 
     ReadOnly Skins$() = {
         "tonacho",
@@ -17,10 +19,34 @@
     Dim nearbyPeds As List(Of Ped)
     Dim distance As Double = 500
 
+    Public ReadOnly Property NextModel As Model
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Get
+            Dim name$ = Skins(rand.Next(0, Skins.Length))
+            Return New Model(name)
+        End Get
+    End Property
+
     Private Sub PedScript_Tick(sender As Object, e As EventArgs) Handles Me.Tick
         Dim nearby As Ped() = World _
             .GetNearbyPeds(Game.Player.Character.Position, distance) _
-            .Where(Function(p) p.RelationshipGroup <> player) _
-            .ToArray
+            .Where(Function(p)
+                       Return p.RelationshipGroup <> player AndAlso nearbyPeds.IndexOf(p) = -1
+                   End Function)
+
+        For Each ped As Ped In nearby
+            If Not ped.IsDead Then
+                Dim location = ped.Position
+                ped.Delete()
+                ped = World.CreatePed(NextModel, location)
+                nearbyPeds.Add(ped)
+            End If
+        Next
+
+        For Each ped As Ped In nearbyPeds.ToArray
+            If ped.IsDead Then
+                nearbyPeds.Remove(ped)
+            End If
+        Next
     End Sub
 End Class
