efb363aae20ed306123e5ae2ec3e9fa701195292
xieguigang <xie.guigang@gmail.com>
2018-10-03 22:50:29 +0800

    add vector helpers from https://github.com/CamxxCore/ScriptCamTool

--- GTA5CinematicCamera/CameraScript.vb
+++ GTA5CinematicCamera/CameraScript.vb
@@ -3,11 +3,34 @@ Imports GTA
 
 Public Class CameraScript : Inherits Script
 
+    Dim toggleOn As Boolean = False
+    Dim rand As New Random
+
     Private Sub CameraScript_Tick(sender As Object, e As EventArgs) Handles Me.Tick
+        If toggleOn Then
+            Call RunCamera()
+        End If
+    End Sub
+
+    Private Sub RunCamera()
 
     End Sub
 
-    Private Sub CameraScript_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+    Private Sub SplineAbove()
 
+    End Sub
+
+    Private Sub FirstPerson()
+
+    End Sub
+
+    Public Function PickRandom() As Ped
+
+    End Function
+
+    Private Sub CameraScript_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.O Then
+            toggleOn = Not toggleOn
+        End If
     End Sub
 End Class
