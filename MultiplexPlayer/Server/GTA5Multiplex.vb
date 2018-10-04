e5e7dc4cae3acac0218c386415a202a120b72135
xieguigang <xie.guigang@gmail.com>
2018-10-05 03:27:23 +0800

    add a basically protocol handler

--- MultiplexPlayer/Server/GTA5Multiplex.vb
+++ MultiplexPlayer/Server/GTA5Multiplex.vb
@@ -1,12 +1,17 @@
-﻿Imports Microsoft.VisualBasic.Net
+﻿Imports GTA5.Multiplex
+Imports Microsoft.VisualBasic.Net
+Imports Microsoft.VisualBasic.Net.Protocols
+Imports Microsoft.VisualBasic.Net.Protocols.Reflection
 
-
+<Protocol(GetType(PlayerControls.Protocols))>
 Public Class GTA5Multiplex
 
     ReadOnly socket As TcpSynchronizationServicesSocket
 
     Sub New(Optional port% = 22335)
-        socket = New TcpSynchronizationServicesSocket(port, AddressOf LogException)
+        socket = New TcpSynchronizationServicesSocket(port, AddressOf LogException) With {
+            .Responsehandler = New ProtocolHandler(Me)
+        }
     End Sub
 
     Private Shared Sub LogException(ex As Exception)
@@ -14,10 +19,11 @@ Public Class GTA5Multiplex
     End Sub
 
     Public Function Run() As Integer
-
+        Return socket.Run
     End Function
 
-    Private Function HandleProtocol()
+    <Protocol(PlayerControls.Protocols.ShootAt)>
+    Public Function ShootAt(request As RequestStream, RemoteAddress As System.Net.IPEndPoint) As RequestStream
 
     End Function
 End Class
