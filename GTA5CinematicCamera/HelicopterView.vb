d3946d0e2d8743b5d030f97eadfedae0e5108f9e
xieguigang <xie.guigang@gmail.com>
2018-10-07 05:23:17 +0800

    enable camera

--- GTA5CinematicCamera/HelicopterView.vb
+++ GTA5CinematicCamera/HelicopterView.vb
@@ -37,7 +37,8 @@ Public Class HelicopterView : Inherits Script
                     .Name = "Camera"
                 End With
 
-                ' pilot.Task.ClearAllImmediately()
+                Call camera.EnterCameraView(helicopter.Position)
+                Call camera.AttachTo(helicopter, New Vector3(0, 0, 0))
             Else
                 ' toggle off
                 Call camera.ExitCameraView
@@ -52,15 +53,13 @@ Public Class HelicopterView : Inherits Script
     Private Sub HelicopterView_Tick(sender As Object, e As EventArgs) Handles Me.Tick
         If (Not pilot Is Nothing) AndAlso last - Now > New TimeSpan(0, 0, 3) Then
             Dim abovePlayer As Vector3 = Game.Player.Character.Position + New Vector3(15, 15, 140)
-            'Dim followPlayer As InputArgument() = New InputArgument() {
-            '    pilot, helicopter, 0, 0,
-            '    abovePlayer.X, abovePlayer.Y, abovePlayer.Z,
-            '    4.0!, 100.0!, 0!, 90.0!, 0, CSng(-200)
-            '}
+            Dim followPlayer As InputArgument() = New InputArgument() {
+                pilot, helicopter, 0, 0,
+                abovePlayer.X, abovePlayer.Y, abovePlayer.Z,
+                4.0!, 100.0!, 0!, 90.0!, 0, CSng(-200)
+            }
 
-            '[Function].Call(Hash._0x23703CD154E83B88, followPlayer)
-            ' pilot.Task.DriveTo(helicopter, abovePlayer, 10, 50)
-            helicopter.Position = abovePlayer
+            [Function].Call(Hash._0x23703CD154E83B88, followPlayer)
 
             last = Now
         End If
