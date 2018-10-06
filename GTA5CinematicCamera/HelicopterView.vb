48075c726fbd64b89233421497f7d642a3d1b9ba
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-10-06 20:27:42 +0800

    using native function calls

--- GTA5CinematicCamera/HelicopterView.vb
+++ GTA5CinematicCamera/HelicopterView.vb
@@ -18,7 +18,7 @@ Public Class HelicopterView : Inherits Script
             If Not camera.IsActive Then
                 ' toggle on
                 Dim model As New Model("buzzard")
-                Dim position As Vector3 = Game.Player.Character.Position + New Vector3(30, 30, 0)
+                Dim position As Vector3 = Game.Player.Character.Position + New Vector3(30, 30, 100)
 
                 helicopter = World.CreateVehicle(model, position)
                 model = New Model("rmiku2016")
@@ -26,9 +26,18 @@ Public Class HelicopterView : Inherits Script
                 pilot.AlwaysKeepTask = True
                 pilot.BlockPermanentEvents = True
                 pilot.DrivingSpeed = 50
-                pilot.DrivingStyle = DrivingStyle.Normal
+                pilot.DrivingStyle = DrivingStyle.Rushed
 
-                pilot.Task.CruiseWithVehicle(helicopter, 50, 0)
+                pilot.AddBlip()
+
+                With pilot.CurrentBlip
+                    .Color = BlipColor.Yellow
+                    .IsFlashing = True
+                    .Scale = 1
+                    .Name = "Camera"
+                End With
+
+                ' pilot.Task.ClearAllImmediately()
             Else
                 ' toggle off
                 Call camera.ExitCameraView
@@ -43,14 +52,14 @@ Public Class HelicopterView : Inherits Script
     Private Sub HelicopterView_Tick(sender As Object, e As EventArgs) Handles Me.Tick
         If (Not pilot Is Nothing) AndAlso last - Now > New TimeSpan(0, 0, 3) Then
             Dim abovePlayer As Vector3 = Game.Player.Character.Position + New Vector3(15, 15, 140)
-            'Dim followPlayer As InputArgument() = New InputArgument() {
-            '    pilot, helicopter, 0, 0,
-            '    abovePlayer.X, abovePlayer.Y, abovePlayer.Z,
-            '    4.0!, 100.0!, 0!, 90.0!, 0, CSng(-200)
-            '}
+            Dim followPlayer As InputArgument() = New InputArgument() {
+                pilot, helicopter, 0, 0,
+                abovePlayer.X, abovePlayer.Y, abovePlayer.Z,
+                4.0!, 100.0!, 0!, 90.0!, 0, CSng(-200)
+            }
 
-            '[Function].Call(Hash._0x23703CD154E83B88, followPlayer)
-            pilot.Task.DriveTo(helicopter, abovePlayer, 10, 50)
+            [Function].Call(Hash._0x23703CD154E83B88, followPlayer)
+            ' pilot.Task.DriveTo(helicopter, abovePlayer, 10, 50)
 
             last = Now
         End If
